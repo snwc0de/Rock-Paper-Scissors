ddd4258f79e5aee46575d4ecb917ca67e2b1ec96
snwc0de <154846385+snwc0de@users.noreply.github.com>
2024-11-11 14:31:18 +0100

    Created the playGame function It calls the playRound function five times

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 console.log("Hello World!")
 
 // COMPUTER CHOICE
-let computerinput = Math.floor(Math.random() * 3);
 let getComputerChoice = function() {
+    let computerinput = Math.floor(Math.random() * 3);
     if (computerinput === 0) {
         return 'Rock';
     } else if (computerinput === 1) {
@@ -28,78 +28,125 @@ function getHumanChoice() {
 
 let humanScore = 0;
 let computerScore = 0
-console.log('UserScore = ' + humanScore);
-console.log('ComputerScore = ' + computerScore);
 
-// function playRound(humanChoice, computerChoice) {
-//     if (humanChoice === computerChoice) {
-//         return 'RESULT: It\'s a tie, Go again.';
-//     }
-
-//     if (humanChoice === 'Rock') {
-//         const humanSelection = getHumanChoice();
-//         const computerSelection = getComputerChoice();
-//         if (computerChoice === 'Paper') {
-//             return 'RESULT: You lose, Paper beats Rock.';
-//         } else if (computerChoice === 'Scissors')
-//             return 'RESULT: You win, Rock beats Scissors.';
-//     }
-
-//     if (humanChoice === 'Paper') {
-//         if (computerChoice === 'Scissors') {
-//             return 'RESULT: You lose, Scissors beats Paper.';
-//         } else if (computerChoice === 'Rock')
-//             return 'RESULT: You win, Paper beats Rock.';
-//     };
-
-//     if (humanChoice === 'Scissors') {
-//         if (computerChoice === 'Paper') {
-//             return 'RESULT: You win, Scissors beats Paper.';
-//         } else if (computerChoice === 'Rock')
-//             return 'RESULT: You lose, Rock beats Scissors.';
-//     };
-
-// }
-// playRound (humanSelection, computerSelection);
-// console.log(playRound (humanSelection, computerSelection))
 
 function playRound(humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
-        console.log ('RESULT: It\'s a tie, Go again.');
-        return 'a tie';
+        return 'RESULT: It\'s a tie.';
     }
     
     if (humanChoice === 'Rock') {
         if (computerChoice === 'Paper') {
-            console.log ('RESULT: You lose, Paper beats Rock.');
-            return 'Computer wins';
-        } else if (computerChoice === 'Scissors')
-            console.log ('RESULT: You win, Rock beats Scissors.');
-            return 'Human wins';
-    }
+            computerScore++;
+            return 'RESULT: You lose, Paper beats Rock.';
+        } else if (computerChoice === 'Scissors'){
+            humanScore++;
+            return 'RESULT: You win, Rock beats Scissors.';
+        }
+    };
 
     if (humanChoice === 'Paper') {
         if (computerChoice === 'Scissors') {
-            console.log ('RESULT: You lose, Scissors beats Paper.');
-            return 'Computer wins';
-        } else if (computerChoice === 'Rock')
-            console.log ('RESULT: You win, Paper beats Rock.');
-            return 'Human wins';
+            computerScore++;
+            return 'RESULT: You lose, Scissors beats Paper.';
+        } else if (computerChoice === 'Rock'){
+            humanScore++;
+            return 'RESULT: You win, Paper beats Rock.';
+        }
     };
 
     if (humanChoice === 'Scissors') {
         if (computerChoice === 'Paper') {
-            console.log ('RESULT: You win, Scissors beats Paper.');
-            return 'Human wins';
-        } else if (computerChoice === 'Rock')
-            console.log ('RESULT: You lose, Rock beats Scissors.');
-            return 'Computer wins';
+            humanScor++;
+            return 'RESULT: You win, Scissors beats Paper.';
+        } else if (computerChoice === 'Rock') {
+            computerScore++;
+            return 'RESULT: You lose, Rock beats Scissors.';
+        }        
     };
-    
 }
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-playRound (humanSelection, computerSelection);
+
+
+function playGame() {
+    console.log(playRound(getHumanChoice(), getComputerChoice()));
+    console.log(playRound(getHumanChoice(), getComputerChoice()));
+    console.log(playRound(getHumanChoice(), getComputerChoice()));
+    console.log(playRound(getHumanChoice(), getComputerChoice()));
+    console.log(playRound(getHumanChoice(), getComputerChoice()));
+}
+playGame()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function gameScore() {
+//     if (getWinner === 'Human wins') {
+//         humanScore++;
+//     } else if (getWinner === 'Computer wins') {
+//         computerScore++;
+//     }
+// } 
+
+
+console.log('UserScore = ' + humanScore);
+console.log('ComputerScore = ' + computerScore);
 
 console.log(humanSelection);
 console.log(computerSelection);
