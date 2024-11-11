a388dfa79a5ad465d3dca9db47797ef32eead6b1
snwc0de <154846385+snwc0de@users.noreply.github.com>
2024-11-11 17:28:18 +0100

    CLEAN CODE Cleaned the code and no changes made to the game logic

--- script.js
+++ script.js
@@ -1,41 +1,46 @@
-console.log("Hello World!")
 
-// COMPUTER CHOICE
+
+// Get computer choice by converting the numbers  gotten at random to strings choices
 let getComputerChoice = function() {
-    let computerinput = Math.floor(Math.random() * 3);
-    if (computerinput === 0) {
+    let computerInput = Math.floor(Math.random() * 3);
+    if (computerInput === 0) {
         return 'Rock';
-    } else if (computerinput === 1) {
+    } else if (computerInput === 1) {
         return 'Paper';
     } else {
         return 'Scissors';
     }
 }
 
-// HUMAN CHOICE
+
+// Get computer choice by prompting users to input their choice with an option to choose from
 function getHumanChoice() {
     let humanInput = prompt('Choose between: \'Rock\', \'Paper\', or \'Scissors\'');
-    let one = 'rock';
-    let two = 'paper';
-    let three = 'scissors';
-    if (humanInput.toLowerCase() === one.toLowerCase()) {
+    
+    let inputOne = 'rock';
+    let inputTwo = 'paper';
+    let inputThree = 'scissors';
+
+    if (humanInput.toLowerCase() === inputOne.toLowerCase()) {
         return 'Rock';
-    } else if (humanInput.toLowerCase() === two.toLowerCase()) {
+    } else if (humanInput.toLowerCase() === inputTwo.toLowerCase()) {
         return 'Paper';
-    } else if (humanInput.toLowerCase() === three.toLowerCase()){
+    } else if (humanInput.toLowerCase() === inputThree.toLowerCase()){
         return 'Scissors';
     } else {
-        alert('Enter a valid input');
+        alert ('Enter a valid input');
     }
 
 }
 
-//SCORE CONTAINER
+
+//These variables containers the gamescores for both players, it will get updated as the games goes on
 let humanScore = 0;
 let computerScore = 0
 
-//A ROUND
-function playRound(humanChoice, computerChoice) {
+
+//Compares the human and computer choice, to increase the score variable of the round winner
+function playRound (humanChoice, computerChoice) {
     if (humanChoice === computerChoice) {
         return 'It\'s a tie.';
     }
@@ -71,26 +76,37 @@ function playRound(humanChoice, computerChoice) {
     };
 }
 
-// FIVE ROUNDS
-function playGame() {
-    console.log('ROUND 1: ' + playRound(getHumanChoice(), getComputerChoice()));
-    console.log('ROUND 2: ' + playRound(getHumanChoice(), getComputerChoice()));
-    console.log('ROUND 3: ' + playRound(getHumanChoice(), getComputerChoice()));
-    console.log('ROUND 4: ' + playRound(getHumanChoice(), getComputerChoice()));
-    console.log('ROUND 5: ' + playRound(getHumanChoice(), getComputerChoice()));
+
+// Calls the playRound function five times to complete the game which should have a total of five rounds
+function playGame () {
+    console.log ('ROUND 1: ' + playRound (getHumanChoice(), getComputerChoice()));
+    console.log ('ROUND 2: ' + playRound (getHumanChoice(), getComputerChoice()));
+    console.log ('ROUND 3: ' + playRound (getHumanChoice(), getComputerChoice()));
+    console.log ('ROUND 4: ' + playRound (getHumanChoice(), getComputerChoice()));
+    console.log ('ROUND 5: ' + playRound (getHumanChoice(), getComputerChoice()));
 }
 playGame();
 
-console.log('UserScore = ' + humanScore);
-console.log('ComputerScore = ' + computerScore);
 
-if (humanScore > computerScore){
-    console.log('FINAL RESULT: THE USER WINS THE GAME');
-} else if (humanScore < computerScore) {
-    console.log('FINAL RESULT: THE COMPUTER WINS THE GAME');
-} else {
-    console.log('FINAL RESULT: THE GAME ENDS IN A DRAW');
+//Collects the total and final game score acquired by the human and computer respectively
+function getFinalGameScores () {
+    console.log ('UserScore = ' + humanScore);
+    console.log ('ComputerScore = ' + computerScore);
 }
+getFinalGameScores();
+
+
+//Compares the final game scores of the human and computer to determine and announce the winner of the game
+function getGameWinner () {
+    if (humanScore > computerScore){
+        console.log ('FINAL RESULT: THE USER WINS THE GAME');
+    } else if (humanScore < computerScore) {
+        console.log ('FINAL RESULT: THE COMPUTER WINS THE GAME');
+    } else {
+        console.log ('FINAL RESULT: THE GAME ENDS IN A DRAW');
+    }
+}
+getGameWinner();
 
 
 
